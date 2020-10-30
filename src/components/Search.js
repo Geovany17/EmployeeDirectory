d310a468a8ddba70b4e1a81c624355c52f7a1823
Geovany Cardoza <geoyd17@gmail.com>
2020-10-29 21:57:27 -0400

    modified all my files , deleted my own employees.json and imags and created a new app basically,

---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Search.css";
+
+const Search = (props) => {
+  return (
+    <div className="d-flex justify-content-center mx-auto">
+      <form>
+        <input
+          placeholder="     Search for employee "
+          name="search"
+          type="text"
+          className="form-control-lg search-font mx-auto"
+          onChange={(event) => props.startSort(event)}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Search;
